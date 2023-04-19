c7afb16207cca9b62af1a66e69ce85811e19c068
vrdark33 <brayan54sd@gmail.com>
2023-04-19 16:21:17 -0500

    se documenta los archivos

--- abb.h
+++ abb.h
@@ -1,3 +1,31 @@
+/*Este es un programa en C++ que implementa un árbol binario de búsqueda (ABB) y ofrece las siguientes funciones:
+
+crearNodo: crea un nuevo nodo en el árbol con el valor proporcionado.
+insertar: inserta un nuevo nodo con el valor proporcionado en el árbol.
+preOrden: imprime los valores de los nodos del árbol en preorden.
+enOrden: imprime los valores de los nodos del árbol en orden.
+postOrden: imprime los valores de los nodos del árbol en postorden.
+verArbol: imprime el árbol de forma gráfica.
+buscar: busca un valor en el árbol y devuelve true si lo encuentra, false en caso contrario.
+eliminar: elimina un nodo con el valor proporcionado del árbol.
+En cuanto a su implementación, se utiliza una estructura nodo que contiene el valor del nodo, 
+así como dos punteros a otros nodos, uno para el hijo izquierdo y otro para el hijo derecho. Además, se define el tipo
+ABB como un puntero a un nodo, para facilitar la manipulación del árbol.
+
+La función insertar es la que se encarga de insertar un nuevo nodo en el árbol, siguiendo la regla del ABB
+de insertar valores menores a la izquierda y valores mayores a la derecha. Las funciones de impresión
+(preOrden, enOrden, postOrden y verArbol) utilizan los algoritmos correspondientes para recorrer
+el árbol y mostrar los valores de los nodos en el orden deseado.
+
+La función buscar recibe un valor a buscar y lo busca en el árbol siguiendo la misma regla del ABB.
+La función devuelve true si el valor se encuentra en el árbol y false en caso contrario.
+
+Finalmente, la función eliminar elimina un nodo con el valor proporcionado del árbol. Para ello, se comprueba
+si el árbol está vacío, si el valor a eliminar es menor o mayor que el valor del nodo actual, o si el nodo
+a eliminar tiene uno o dos hijos. En función de cada caso, se toman distintas acciones para mantener la estructura
+del árbol.*/
+
+
 #include<iostream>
 #include<stdio.h>
 #include<stdlib.h>
--- claseArbol.cpp
+++ claseArbol.cpp
@@ -1,3 +1,40 @@
+/*Este es un programa en C++ que implementa un menú para manejar un Árbol Binario de Búsqueda (ABB). 
+El código incluye un archivo de encabezado "abb.h" que contiene las declaraciones de las funciones necesarias 
+para operar el árbol.
+La función main() implementa el menú mediante un ciclo do-while. En cada iteración, 
+se muestra el menú y se lee la opción elegida por el usuario mediante la variable op. 
+Luego, se ejecuta la acción correspondiente según la opción elegida.
+A continuación, se describe brevemente cada opción del menú:
+
+insertar un nodo: se lee un número entero desde la entrada estándar y se inserta en el árbol mediante
+la función insertar().
+
+eliminar un nodo: se lee un número entero desde la entrada estándar y se elimina
+del árbol mediante la función eliminar().
+
+buscar un nodo: se lee un número entero desde la entrada estándar y se busca en el árbol mediante
+la función buscar(). Si se encuentra el número, se imprime un mensaje indicando que se ha encontrado; de lo contrario,
+se imprime un mensaje indicando que no se ha encontrado.
+
+mostrar el arbol: se imprime el árbol completo mediante la función verArbol().
+
+recorrer el arbol en EN-orden: se recorre el árbol en orden ascendente y se imprime cada nodo
+visitado mediante la función enOrden().
+
+recorrer el arbol en PRE-orden: se recorre el árbol en preorden y se imprime cada nodo visitado
+mediante la función preOrden().
+
+recorrer el arbol en POST-orden: se recorre el árbol en postorden y se imprime cada nodo visitado
+mediante la función postOrden().
+
+salir: se sale del programa.
+
+Después de ejecutar cada opción del menú, se limpia la pantalla mediante la función system("cls") y se espera a
+que el usuario presione cualquier tecla para continuar mediante la función system("pause").
+
+En resumen, este programa implementa un menú para manejar un Árbol Binario de Búsqueda y proporciona
+varias opciones para operar el árbol.*/
+
 #include<iostream>
 #include<stdio.h>
 #include<stdlib.h>
